2e7fff2ac904d8c9f6993598fcd046dcdce07326
Théomé Borck <theome.borck@student.ecp.fr>
2018-06-28 14:36:13 +0200

    Update snapshot

--- src/containers/AddScore.js
+++ src/containers/AddScore.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { addPlayer } from '../Actions'
-import HighScoreInput from '../components/HighScoreInput'
+
 
 
 let AddScore = ({ dispatch }) => {
